8955d71d67c83bfb387c9e75f649cb30f0b8375a
ikmarokko2 <35876458+ikmarokko2@users.noreply.github.com>
2018-10-11 10:01:26 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,9 @@
+    /**
+     * use the Discord.js library which we installed with npm
+     */
+    const Discord = require('discord.js');
+    
+    /**
+     * initialize client
+     */
+    const client = new Discord.Client();
