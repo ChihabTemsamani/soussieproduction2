20454e7a9f779edd9c598ae1640542339a860ed7
ikmarokko2 <35876458+ikmarokko2@users.noreply.github.com>
2018-10-11 10:07:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,3 +7,16 @@
      * initialize client
      */
     const client = new Discord.Client();
+/**
+ * Use `client.on` to handle discord events
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+/**
+ * add your token here to connect bot
+ * keep this line at end of file to make sure your bot has read
+ * the entire script before going online
+ */
+client.login('BOT TOKEN');
